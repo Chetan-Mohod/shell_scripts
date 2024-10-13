b30a3b3eefededb9b3f43ae0b3f38192f5269caa
Chetan Mohod <55320509+Chetan-Mohod@users.noreply.github.com>
2024-10-13 23:53:48 +0530

    Create user_input_script.sh

---
+++ user_input_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+<< note
+This script will take the input from user
+note
+
+read -p "Enter the name:" name
+echo "My name is $name"
+echo "What is your name?"
+read name2
+echo "My name is $name2"
